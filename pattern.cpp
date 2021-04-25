33b8af6f67ccdc5a4873e398c3c5c864239188c6
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-04-25 19:16:23 +0530

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n,i,j;
+	cin>>n;
+for(i=1;i<=n;i++){
+	for(j=1;j<=2*n;j++){
+		if(j<=i||j>(2*n-i)){
+		cout<<"*";
+		}
+	else{
+		cout<<"#";
+	}
+	}
+	cout<<"\n";
+	}
+}
+	
